1c9b3e125447bd75604e2659549db03fe97978d3
joalvarado2 <jonathanalva92@gmail.com>
2023-04-26 18:48:52 -0500

    se establece la conexion a base de datos por medio de docker y se crean los DTO para realizar el post del pokemon

--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,12 @@
-export class CreatePokemonDto {}
+import { IsIn, isPositive, MinLength, IsString, Min } from 'class-validator';
+
+export class CreatePokemonDto {
+  //  @IsIn()
+  //  @isPositive()
+  @Min(1)
+  numero: number;
+
+  @IsString()
+  @MinLength(1)
+  name: string;
+}
